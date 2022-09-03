4ab338b65df8d53bd88c0ee62075a971d75bfecb
astange <astange@users.noreply.github.com>
2022-09-03 12:14:54 +0530

    lemonades: sensors-multihal: Update to 2.1
    Bug: 149758467
    Test: Verify that the new unit tests and subhals in this topic load and
    run on a Pixel device
    
    Change-Id: I275274659816b3a00374fb0b7a93f199874afdcd
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -379,7 +379,11 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.1-service.multihal_oplus
+
+PRODUCT_PACKAGES += \
     libsensorndkbridge \
     sensors.oplus
 
